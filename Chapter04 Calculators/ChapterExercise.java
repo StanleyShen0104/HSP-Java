335e7da40974ba999f4e8c32697e2e185d2382db
DESKTOP-IGIR3K1\Mr.Cash <eziorial@gmail.com>
2023-12-14 13:35:03 +0900

    Stanley.Shen

---
+++ Chapter04 Calculators/ChapterExercise.java
@@ -0,0 +1,14 @@
+public class ChapterExercise {
+	public static void main(String arg[]) {
+		/* 10 / 3 = 3
+		 *	10 / 5 = 2
+		 *	10 % 2 = 0
+		 *	-10.5 % 3 = -1.5 
+		 */
+		
+		/*int i =66;
+		 * System.out.println(++i+i);
+		 *	ans:134
+		 * */
+	}
+}
