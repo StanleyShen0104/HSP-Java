0cccb12a4a12d572cb924ae3e5d7eb7561feba59
DESKTOP-IGIR3K1\Mr.Cash <eziorial@gmail.com>
2023-12-28 13:56:58 +0900

    Stanley.Shen

--- Chapter07 ClassObject/EightQueens.java
+++ Chapter07 ClassObject/EightQueens.java
@@ -3,74 +3,83 @@ public class EightQueens {
 	public static void main(String[] args) {
 		//8 chess cannot same row col line, each row col line only can be one 
 		Chess c = new Chess();
-		int[][] chess = c.genrMap();
-		c.trying(chess, 0, 0);
-		c.draw(chess);
-	}
-		
+		c.solve(0);
+	}	
 }
 
 class Chess {
-	public boolean trying(int[][] map , int x, int y) {
-		//0 uncheck; 1 right place; 2 wrong place
+	int[] queen = new int[8];
+	int count;
+	
+	public void solve(int row) {
 		/*	
-		 *	check
-		 *	check y -> up iteration
-		 *	check n -> 2 -> right iteration
-		 *	
+		 *	int []queenArr;
+		 *	i->queen's rowIndex;
+		 *	queenArr[i]->queen's column Index
+		 *
+		 *	4 keys recursion situation:
+		 *		1.vaild -> next row recursion
+		 *		2.vaild -> next row reach limit -> exit recursion, not exit method
+		 *		3.invaild -> next column recursion
+		 *		4.invaild -> column iteration all invalid; ->column loop end, exit recursion
 		 */
-		//remain unsolved
+		
+		//PS: recursion exit always code first
+		//row == 8 out of index range
+		if (row == 8) {
+			draw();
+			count++;
+			return;
+		}
+		
+		for (int col = 0; col < 8; col++) {
+			if (isPass(row, col)) {
+				queen[row] = col;
+				solve(row + 1);
+			}
+		}
 	}
 	
-	public void draw(int[][] chess) {
-		System.out.println("---Drawing---");
-		for (int i = 0; i < chess.length; i++) {
-			for (int j = 0; j < chess[i].length; j++) {
-				System.out.print(chess[i][j] + " ");
+	public void draw() {
+		String str =  (count < 10) ? "0"+count: Integer.toString(count);
+		System.out.println("---Drawing" + str + "---");
+		for (int i = 0; i < 8; i++) {
+			for (int j = 0; j < 8; j++) {
+				if (j == queen[i]) {
+					System.out.print("* ");
+				}
+				else {
+					System.out.print("- ");
+				}
 			}
 			System.out.println();
 		}
+		System.out.println();
 	}
-	
-	public int[][] genrMap() {
-		int[][] map = new int[8][8];
-		return map;
-	}
-	
-	public boolean isPass(int[][] map, int x, int y) {
-		for (int i = 0; i < 8; i++) {
-			//same row against rule
-			if (map[x][i] == 1 && i != y) {
+
+	public boolean isPass(int row, int col) {
+		//	i < row is necessary. otherwise, there will be always return false
+		for (int i = 0; i < row; i++) {
+			/*
+			 *	Invaild judgement:
+			 * 
+			 * 		---	---	---	0,3		
+			 * 		1,0	---	---	---
+			 * 		---	2,1	---	---
+			 * 		3,0	---	3,2	---
+			 * 
+			 *		1.same columns -> columnIndex are same
+			 *		2.same left slash -> columnIndex - rowIndex are same
+			 *		3.same right slash -> columnIndex + rowIndex are same
+			 */
+			if(col == queen[i] || col - row == queen[i] - i || col + row == queen[i] + i) {
 				return false;
 			}
-			//same colums against rule
-			else if (map[i][y] == 1 && i != x) {
-				return false;
-			}
-		}
-		int buffer = 1;
-		//up line against rule
-		while(x + buffer < 8) {
-			if (y + buffer < 8 && map[x + buffer][y + buffer] == 1) {
-				return false;
-			}
-			if (y - buffer >= 0 && map[x + buffer][y - buffer] == 1) {
-				return false;
-			}
-			buffer++;
-		}
-		
-		buffer = 1; //reset buffer
-		//down line against rule
-		while(x - buffer >= 0) {
-			if (y + buffer < 8 && map[x - buffer][y + buffer] == 1) {
-				return false;
-			}
-			if (y - buffer >= 0 && map[x - buffer][y - buffer] == 1) {
-				return false;
-			}
-			buffer++;
 		}
 		return true;
 	}
 }
+
+
+
+
